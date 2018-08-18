c4e2e192000535aa56c3a5fec811eea650c063f3
FunNyLuAz <ladsword@gmail.com>
2018-08-18 15:48:51 -0300

    Updated

--- Hyper Codificator.py
+++ Hyper Codificator.py
@@ -1,6 +1,7 @@
-cunit = ''
 cunitl = []
 templ = []
+
+msg = input('Digite o texto: ')
 
 print('''
 Escolha a ordem de codificação:
@@ -13,22 +14,45 @@ Escolha a ordem de codificação:
 [ 6 ] Caesar
 [ 7 ] ROT-13
 [ 8 ] Base 64
-[ 9 ] Upper or Lower Case (9+: Upper | 9-: Lower)''')
+[ 9 ] Upper Case''')
 
 while True:
-    cunit = input('\nInsira a ordem: ')
+    cunit = input('\nInsira a ordem de codificação: ')
     if cunit.isnumeric() is True:
         for c in cunit:
-            cunitl.append(c)
+            cunitl.append(int(c))
         for c in cunit:
-            if c not in templ:
-                templ.append(c)
+            if int(c) not in templ:
+                templ.append(int(c))
             else:
                 break
-        if templ == cunitl:
+        if templ[:] == cunitl[:]:
+            templ.clear()
             break
     print('\033[4;31mValor Inválido. Tente Novamente\033[m')
+    templ.clear()
+    cunitl.clear()
 
+codec = msg
 
+for p, c in enumerate(cunitl):
+    if c == 1:
+        ord(codec)
+    elif c == 2:
+        hex(codec)
+    elif c == 3:
+        bin(codec)
+    elif c == 4:
+        reversed(codec)
+    '''elif c == 5:
 
-print(cunitl)
+    elif c == 6:
+
+    elif c == 7:
+
+    elif c == 8:
+        
+    elif c == 9:
+        codec.upper()'''
+
+print(codec)
